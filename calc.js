a9b06ae32c236d76ccf77f32784c7f7a645ab389
Venkat <75605991+venkatt05@users.noreply.github.com>
2023-03-02 16:35:43 +0530

    Init commit

---
+++ calc.js
@@ -0,0 +1,32 @@
+const operations = {
+  ADDITION: (n1, n2) => n1 + n2,
+  SUBRACTION: (n1, n2) => n1 - n2,
+  MULTIPLICATION: (n1, n2) => n1 * n2,
+  DIVISION: (n1, n2) => n1 / n2,
+  PERCENTAGE: (n1, n2) => (n1 * n2) / 100,
+};
+
+const validateInputs = (n1, n2, func) => {
+  if ((typeof n1 !== 'number') || (typeof n2 !== 'number'))
+  {
+    throw "Invalid Inputs";
+  }
+  return func(n1, n2);
+};
+
+const printAvailableOperations = function () {
+  return Object.keys(operations);
+};
+
+const bbCalculator = {
+  calc: {
+    add: (n1, n2) => validateInputs(n1, n2, operations.ADDITION),
+    sub: (n1, n2) => validateInputs(n1, n2, operations.SUBRACTION),
+    divide: (n1, n2) => validateInputs(n1, n2, operations.DIVISION),
+    multiply: (n1, n2) => validateInputs(n1, n2, operations.MULTIPLICATION),
+    percent: (n1, n2) => validateInputs(n1, n2, operations.PERCENTAGE),
+  },
+  printAvailableOperations,
+};
+
+module.exports = bbCalculator;
